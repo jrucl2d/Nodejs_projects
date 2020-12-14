a8f273e19522055e494d9bf1b25b2be4dae1a771
jrucl2d <ysk789465@gmail.com>
2020-12-14 16:26:00 +0900

    Cors 문제 해결, apilimiter 분기처리

--- nodejsTextbook-2Cycle/ch10/ggbook-api/routes/v2.js
+++ nodejsTextbook-2Cycle/ch10/ggbook-api/routes/v2.js
@@ -4,14 +4,58 @@ const Domain = require("../models/Domain");
 const User = require("../models/User");
 const Post = require("../models/Post");
 const Hashtag = require("../models/Hashtag");
+const url = require("url");
+const cors = require("cors");
 
-const { verifyToken, apiLimiter } = require("./middlewares");
+const { verifyToken, apiLimiter, premiumApiLimiter } = require("./middlewares");
 
-router.post("/token", apiLimiter, async (req, res, next) => {
-  const { serverSecret } = req.body;
+router.use(async (req, res, next) => {
+  try {
+    // 미리 설정한 도메인만 cors 접근 가능
+    const domain = await Domain.findOne({
+      where: {
+        host: url.parse(req.get("origin"))
+          ? url.parse(req.get("origin")).host
+          : undefined,
+      }, // optional chaining. 앞에가 undefined면 undefined이고 있으면 그 안에서 host 꺼내옴
+    });
+    if (domain) {
+      cors({
+        origin: req.get("origin"), // credentials가 들어가면 여기 *가 안 된다. 그래서 true를 쓰면 현재 도메인이 가능하다.
+        credentials: true, // 쿠키가 같이 전달 안 되는 문제를 해결하기 위해서 넣어줌
+      })(req, res, next);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.use(async (req, res, next) => {
   try {
     const domain = await Domain.findOne({
-      where: { serverSecret },
+      where: { host: url.parse(req.get("origin")).host },
+    });
+    if (domain.type == "free") {
+      console.log("무료 사용자 입니다");
+      apiLimiter(req, res, next);
+    } else {
+      console.log("프리미엄 사용자 입니다");
+      premiumApiLimiter(req, res, next);
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post("/token", async (req, res, next) => {
+  const { frontSecret } = req.body;
+  try {
+    const domain = await Domain.findOne({
+      where: { frontSecret },
       include: {
         model: User,
         attribute: ["nick", "id"],
@@ -48,7 +92,7 @@ router.post("/token", apiLimiter, async (req, res, next) => {
   }
 });
 
-router.get("/test", verifyToken, apiLimiter, (req, res) => {
+router.get("/test", verifyToken, (req, res) => {
   res.json(req.decoded);
 });
 
@@ -67,31 +111,26 @@ router.get("/posts/my", verifyToken, apiLimiter, async (req, res, next) => {
 });
 
 // 해시태그로 게시글을 검색하는 라우터
-router.get(
-  "/posts/hashtag/:title",
-  verifyToken,
-  apiLimiter,
-  async (req, res, next) => {
-    try {
-      const hashtag = await Hashtag.findOne({
-        where: { title: req.params.title },
-      });
-      if (!hashtag) {
-        return res.status(404).json({
-          code: 404,
-          message: "검색 결과가 없습니다",
-        });
-      }
-      const posts = await hashtag.getPosts();
-      return res.json({ code: 200, payload: posts });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({
-        code: 500,
-        message: "서버 에러",
+router.get("/posts/hashtag/:title", verifyToken, async (req, res, next) => {
+  try {
+    const hashtag = await Hashtag.findOne({
+      where: { title: req.params.title },
+    });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: "검색 결과가 없습니다",
       });
     }
+    const posts = await hashtag.getPosts();
+    return res.json({ code: 200, payload: posts });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러",
+    });
   }
-);
+});
 
 module.exports = router;
