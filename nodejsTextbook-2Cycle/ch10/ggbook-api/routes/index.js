aa0af0c31c73866cb7b704205c943dde1646f661
jrucl2d <ysk789465@gmail.com>
2020-12-14 12:46:44 +0900

    api 서버 init

---
+++ nodejsTextbook-2Cycle/ch10/ggbook-api/routes/index.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { V4: uuid } = require("uuid");
+const Domain = require("../models/Domain");
+const User = require("../models/User");
+const { isLoggedIn } = require("./middlewares");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: (req.user && req.user.id) || null },
+      include: { model: Domain },
+    });
+    res.render("login", {
+      user,
+      domains: user && user.Domains,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post("/domain", isLoggedIn, async (req, res, next) => {
+  try {
+    await Domain.create({
+      UserId: req.user.id,
+      host: req.body.host,
+      type: req.body.type,
+      serverSecret: uuid(),
+      frontSecret: uuid(),
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
