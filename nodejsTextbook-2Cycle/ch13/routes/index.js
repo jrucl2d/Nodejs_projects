f9dcdc9e37a43ab001883cc30ba32ad09f26c203
jrucl2d <ysk789465@gmail.com>
2020-12-16 19:10:58 +0900

    경매 낙찰 내역 확인 추가

--- nodejsTextbook-2Cycle/ch13/routes/index.js
+++ nodejsTextbook-2Cycle/ch13/routes/index.js
@@ -174,4 +174,19 @@ router.post("/good/:id/bid", isLoggedIn, async (req, res, next) => {
   }
 });
 
+// 낙찰 결과 확인
+router.get("/list", isLoggedIn, async (req, res, next) => {
+  try {
+    const goods = await Good.findAll({
+      where: { SoldId: req.user.id },
+      include: { model: Auction },
+      order: [[{ model: Auction }, "bid", "DESC"]],
+    });
+    res.render("list", { title: "낙찰 목록", goods });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;
